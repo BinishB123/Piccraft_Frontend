0cd459f6e9b1a28d6bf87e923fcdbf1ce25c4070
BinishB123 <binishb2018@gmail.com>
2025-02-09 23:26:47 +0530

    l=0

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiUrl = import.meta.env.VITE_BACKEND_BASE_URL 
+export const apiUrl = "https://piccraft-backend-tg36.vercel.app"
 
 export const axiosInstance = axios.create({
   baseURL: apiUrl,
